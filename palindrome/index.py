10c1bac3341786432922b6087108e4e1726f9557
KonkenBonken <konrad05pettersson@gmail.com>
2023-04-27 18:51:56 +0200

    Ignore whitespaces using regex

--- palindrome/index.py
+++ palindrome/index.py
@@ -1,7 +1,12 @@
+from re import compile as regex, sub
+
+
 def isPalindrome(string: str):
+    string = sub(regex('\s'), '', string)
     return string == string[::-1]
 
 
 assert isPalindrome("hello") == False
 assert isPalindrome("kajak") == True
 assert isPalindrome("abccba") == True
+assert isPalindrome("ni talar bra latin") == True
