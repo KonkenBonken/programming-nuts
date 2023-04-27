dcf9e69816f83a512eade5d31d9567257ea4d7b1
KonkenBonken <konrad05pettersson@gmail.com>
2023-04-27 18:53:19 +0200

    Ignore casing

--- palindrome/index.py
+++ palindrome/index.py
@@ -2,11 +2,11 @@ from re import compile as regex, sub
 
 
 def isPalindrome(string: str):
-    string = sub(regex('\s'), '', string)
+    string = sub(regex('\s'), '', string).lower()
     return string == string[::-1]
 
 
 assert isPalindrome("hello") == False
 assert isPalindrome("kajak") == True
 assert isPalindrome("abccba") == True
-assert isPalindrome("ni talar bra latin") == True
+assert isPalindrome("Ni talar bra latin") == True
